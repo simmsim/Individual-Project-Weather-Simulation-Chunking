0e564cdc12b316368e7158bf81eee27832347103
simmsim <simona.paulau@gmail.com>
2021-11-30 16:43:49 +0000

    Reintegrate chunk back to array using std::copy

--- chunking_test.cpp
+++ chunking_test.cpp
@@ -5,6 +5,9 @@
 #include <stdlib.h>
 #include <iostream>
 
+void computePeriodic(float * ch1);
+void computeCore(float * ch1, float * ch2);
+
 void fillAndChunk(float *inputArray, float *outputAray) {
     int iDim = 4;
     int jDim = 4;
@@ -27,9 +30,10 @@ void fillAndChunk(float *inputArray, float *outputAray) {
             for (int i_start = 0; i_start < iDim; i_start += iChunk) {  //i_start = 2
                 // input chunk
                 float *ch1 = (float*)malloc(sizeof(float)*chHSize);
-                // output chunk
-                float *ch2 = (float*)malloc(sizeof(float)*chSize);
                 std::fill(ch1, ch1+(chHSize), 0.0);
+                // output chunk
+                float *ch2 = (float*)malloc(sizeof(float)*chHSize);
+                std::fill(ch2, ch2+(chHSize), 0.0);
 
                 for (int k = 0; k < kHalChunk; k++) { // k = 0
                     if ((k_start == 0 && k == 0) ||
@@ -59,7 +63,7 @@ void fillAndChunk(float *inputArray, float *outputAray) {
                                 int chunkIdx = k*iHalChunk*jHalChunk + i*jHalChunk + j;
                             #endif
 
-                            ch1[chunkIdx] = inputArray[inputArrayIdx]*2;
+                            ch1[chunkIdx] = inputArray[inputArrayIdx];
                             // Once ch1 is filled with values and correctly padded with halo, send it to kernel...
                             // there, haloes will be updated accordingly based on appropriate boundary conditions
                             // calculations on core values will be places to ch2, and ch2 will be coppied back
@@ -68,25 +72,52 @@ void fillAndChunk(float *inputArray, float *outputAray) {
                     }
                 }
 
+                computePeriodic(ch1);
+
                 // print out chunk
                 for (int idx = 0; idx < chHSize; idx++) {
                     std::cout << ch1[idx] << " ";
                 }
                 std::cout << "\n\n" << "";
+                
+                // Then compute something (increment by 1 or smt...)
+               
+                computeCore(ch1, ch2);
+                std::cout << "\nCore\n" << "";
+                for (int idx = 0; idx < chHSize; idx++) {
+                    std::cout << ch2[idx] << " ";
+                }
+                std::cout << "\n\n" << "";
+                
 
                 // integrate chunk into output_array
                 // this is done assuming that the
                 for (int k = 1; k < kHalChunk - 1; k++) { // k = 0
                     for (int j = 1; j < jHalChunk - 1; j++) {
-                        for (int i = 1; i < iHalChunk - 1; i++) {
-                            int chunkIdx = k*iHalChunk*jHalChunk + j*iHalChunk + i;
-                            int outputArrayIdx = (k-1+k_start)*iDim*jDim + (j-1+j_start)*iDim + (i-1+i_start);
-                            // this ch1 should actually be ch2 with only core values, without halo; with no halo
-                            // we could just copy the hole thing, not point by point
-                            outputAray[outputArrayIdx] = ch1[chunkIdx];
-                        }
+                       // for (int i = 1; i < iHalChunk - 1; i++) {
+                            int chunkIdx = k*iHalChunk*jHalChunk + j*iHalChunk + 1;
+                            // j =1; chunkIdx = 24 + 4 + 1 = 29
+                            // j =2; chunkIx = 24 + 8 + 1= 33
+                            //int outputArrayIdx = (k-1+k_start)*iDim*jDim + (j-1+j_start)*iDim + (i-1+i_start);
+                            int first = ch2[chunkIdx];
+                            int second = ch2[chunkIdx+iChunk];
+                            int arrayEnd = i_start + (j-1)*iChunk*2 + (k-1)*iChunk*jChunk*2; // 2 should be no of chunks
+                            std::copy(ch2 + chunkIdx, ch2 + chunkIdx + iChunk, outputAray+arrayEnd);
+                            int outputArraySize = iDim*jDim*kDim;
+
+                            std::cout << "\n Ouput array at " << j << "\n";
+                            for (int x = 0; x < outputArraySize; x++) {
+                                float number = outputAray[x];
+                                std::cout << number << " ";
+                            }
+                            std::cout << "end of output array\n" << "";
+                           // outputAray[outputArrayIdx] = ch1[chunkIdx];
+                        //}
                     }
                 }
+
+                //for (int x = (iHalChunk*jHalChunk)+iHalChunk+1; x < 96-iHalChunk*jHalChunk; x+)
+
                 /*
                 int chunkSize = iChunk*jChunk*kChunk;
                 for (int idx = 0; idx < chunkSize; idx++) {
@@ -95,6 +126,60 @@ void fillAndChunk(float *inputArray, float *outputAray) {
                 */
 
                 free(ch1);
+                free(ch2);
+            }
+        }
+    }
+}
+
+unsigned int FTNREF3D0(int ix, int jx, int kx, unsigned int iz,unsigned int jz) {
+        return iz*jz*kx+iz*jx+ix ;
+}
+
+void computePeriodic(float * ch1) {
+    int iHalChunk = 4;
+    int jHalChunk = 6;
+    int kHalChunk = 4;
+    unsigned int ip = iHalChunk - 2;
+    unsigned int jp = jHalChunk - 2;
+
+    // on real kernel there will be no loop like here
+    // In the real kernel, int i = get_globa_id(0), int j = get_global_id(1)...
+    for (int k = 0; k < kHalChunk; k++) {
+        for (int j = 0; j < jHalChunk; j++) {
+            for (int i = 0; i < iHalChunk; i++) {
+                ch1[FTNREF3D0(i,0,k, ip+2, jp+2)] = ch1[FTNREF3D0(i,jp,k, ip+2, jp+2)];
+                ch1[FTNREF3D0(i,jp+1,k, ip+2, jp+2)] = ch1[FTNREF3D0( i,1,k, ip+2, jp+2)];
+            }
+        }
+    }
+}
+
+unsigned int F3D2C(unsigned int i_rng,unsigned int j_rng, // ranges, i.e. (hb-lb)+1
+        int i_lb, int j_lb, int k_lb, // lower bounds
+        int ix, int jx, int kx
+        ) {
+    return (i_rng*j_rng*(kx-k_lb)+i_rng*(jx-j_lb)+ix-i_lb);
+}
+
+void computeCore(float * ch1, float * ch2) {
+    int iHalChunk = 4;
+    int jHalChunk = 6;
+    int kHalChunk = 4;
+    unsigned int ip = iHalChunk - 2;
+    unsigned int jp = jHalChunk - 2;
+
+    // on real kernel there will be no loop like here
+    // In the real kernel, int i = get_globa_id(0), int j = get_global_id(1)...
+    for (int k = 1; k < kHalChunk-1; k++) {
+        for (int j = 1; j < jHalChunk-1; j++) {
+            for (int i = 1; i < iHalChunk-1; i++) {
+                ch2[F3D2C(ip+2, jp+2, 0,0,0, i,j,k)] = ch1[F3D2C(ip+2, jp+2, 0,0,0, i-1,j,k)]
+                                                    + ch1[F3D2C(ip+2, jp+2, 0,0,0, i+1,j,k)]
+                                                    + ch1[F3D2C(ip+2, jp+2, 0,0,0, i,j-1,k)]
+                                                    + ch1[F3D2C(ip+2, jp+2, 0,0,0, i,j+1,k)]
+                                                    + ch1[F3D2C(ip+2, jp+2, 0,0,0, i,j,k-1)]
+                                                    + ch1[F3D2C(ip+2, jp+2, 0,0,0, i,j,k+1)];
             }
         }
     }
--- sor_kernel.cl
+++ sor_kernel.cl
@@ -4,52 +4,6 @@
 #define TOPBOTTOM 4
 #define CORE 5
 
-// Calculates the position, evaluated by the effect of the loop ordering
-inline int4 calc_loop_iters(int idx, int im, int jm, int km, int i_off, int j_off, int k_off) 
-{
-    int4 ijk;
-    
-#if LOOP_ORDER == 1     
-    // jki 
-    int ik_range = km*im;
-    ijk.s1=  j_off + idx/ik_range; //j
-    ijk.s2 = k_off + idx % ik_range / im; //k
-    ijk.s0 = i_off + idx % im; //i
-#elif LOOP_ORDER == 2
-    // ijk: j -> i, k -> j, i-> k
-    int kj_range = km*jm;
-    ijk.s0=  i_off + idx/kj_range; //i
-    ijk.s1 = j_off + idx % kj_range / km; //j
-    ijk.s2 = k_off + idx % km; //k
-#elif LOOP_ORDER == 3 // This cause an out-or-resources error for Phys1!
-    // kij: j -> k, k -> i, i -> j 
-    int ji_range = jm*im;
-    ijk.s2=  k_off + idx/ji_range; //k
-    ijk.s0 = i_off + idx % ji_range / jm; //i
-    ijk.s1 = j_off + idx % jm; //j
-#elif LOOP_ORDER == 4
-    // jik: j -> j, k -> i, i-> k
-    int ik_range = km*im;
-    ijk.s1=  j_off + idx/ik_range; //j
-    ijk.s0 = i_off + idx % ik_range / km; //i
-    ijk.s2 = k_off + idx % km; //k
-#elif LOOP_ORDER == 5
-    // ikj: j->i, k->k,i->j 
-    int jk_range = km*jm;
-    ijk.s0=  i_off + idx/jk_range; //i
-    ijk.s2 = k_off + idx % jk_range / jm; //k
-    ijk.s1 = j_off + idx % jm; //j
-#elif LOOP_ORDER == 6
-    // kji: j->k,k->j,i->i    
-    int ij_range = jm*im;
-    ijk.s2=  k_off + idx/ij_range; //k
-    ijk.s1 = j_off + idx % ij_range / im; //j
-    ijk.s0 = i_off + idx % im; //i
-#endif    
-    return ijk;
-}
-
-// From 4D in Fortran to 1D in C
 inline unsigned int F4D2C(unsigned int i_rng,unsigned int j_rng, unsigned int k_rng, // ranges, i.e. (hb-lb)+1
                           int i_lb, int j_lb, int k_lb, int l_lb, // lower bounds
                           int ix, int jx, int kx, int lx) 
@@ -59,6 +13,10 @@ inline unsigned int F4D2C(unsigned int i_rng,unsigned int j_rng, unsigned int k_
             i_rng*(jx-j_lb)+
             ix-i_lb
             );
+}
+
+inline unsigned int FTNREF3D0(int ix, int jx, int kx, unsigned int iz,unsigned int jz) {
+        return iz*jz*kx+iz*jx+ix ;
 }
 
 /* 
@@ -71,29 +29,27 @@ Compute the periodic condition, i.e. initialize side halos
 p_in(0, i, 0, k) = p_in(0, i, jp, k)
 p_in(0, i, jp+1, k) = p_in(0, i, 1, k)
 */
-void compute_periodic_condition(__global float *p_in, const int iHalChunk, 
-                                const int jHalChunk, const int kHalChunk) 
+void compute_periodic_condition(__global float *p, const int iChunk, 
+                                const int jChunk, const int kChunk) 
 {
     // How do we know that we're on the side of the chunk?
     // - When a point is on halo: j = 0 || j = jp + 1(given it's a 1-point halo, 
-    //  TODO: check with supervisor if the solution should be generalized to n-point halo)
     // - We'll do pointless periodic boundary calculations for bottom and top 
     //  hallo planes, so that we don't need to do additional checks.
-    // - Need to get i, j, k position first!
     
     int global_id = get_global_id(0);
-    int4 ijk = calc_loop_iters(global_id, iHalChunk, jHalChunk, kHalChunk, 0, 0, 0);
-    unsigned int i = ijk.s0;
-    unsigned int j = ijk.s1;
-    unsigned int k = ijk.s2;
+    unsigned int ip = iChunk;
+    unsigned int jp = jChunk;
 
-    unsigned int ip = iHalChunk - 2;
-    unsigned int jp = jHalChunk - 2;
+    int i_range = iChunk + 2;
+    int j_range = jChunk + 2;
+    int k_range = kChunk + 2;
+    
+    int k = global_id/(i_range*j_range);
+    int i = global_id-(k*i_range);
 
-    if (j == 0 || j == jp + 1) {
-        p_in[F4D2C(((1 - 0)+1), (((ip+2) - 0)+1), (((jp+2) - 0)+1), 0,0,0,0, 0,i,0,k)] = p_in[F4D2C(((1 - 0)+1), (((ip+2) - 0)+1), (((jp+2) - 0)+1), 0,0,0,0, 0,i,jp,k)];
-        p_in[F4D2C(((1 - 0)+1), (((ip+2) - 0)+1), (((jp+2) - 0)+1), 0,0,0,0, 0,i,jp+1,k)] = p_in[F4D2C(((1 - 0)+1), (((ip+2) - 0)+1), (((jp+2) - 0)+1), 0,0,0,0, 0,i,1,k)];
-    }
+    p[FTNREF3D0(i, 0, k, ip+2, jp+2)] = p[FTNREF3D0(i, jp, k, ip+2, jp+2)];
+    p[FTNREF3D0(i, jp+1, k, ip+2, jp+2)] = p[FTNREF3D0(i, 1, k, ip+2, jp+2)];
 }
 
 /*
