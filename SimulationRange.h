9729c3c2625aae94d69bee808862d0668391541a
simmsim <simona.paulau@gmail.com>
2021-12-09 16:02:54 +0000

    Not provided dimensions are set to 1 as this is needed for the chunking logic

--- SimulationRange.h
+++ SimulationRange.h
@@ -14,15 +14,15 @@ class SimulationRange {
 
         SimulationRange(int dim0) : dimensions(1) {
             dimSizes[1] = dim0;
-            dimSizes[1] = 0;
-            dimSizes[2] = 0;
+            dimSizes[1] = 1;
+            dimSizes[2] = 1;
             simulationSize = dim0;
         }
 
         SimulationRange(int dim0, int dim1) : dimensions(2) {
             dimSizes[1] = dim0;
             dimSizes[1] = dim1;
-            dimSizes[2] = 0;
+            dimSizes[2] = 1;
             simulationSize = dim0 * dim1;
         }
 
