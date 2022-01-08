e63a7c41da211560071dd58b690fd81b58ee0b08
simmsim <simona.paulau@gmail.com>
2022-01-08 18:09:31 +0000

    Add new test helper methods

--- test_helpers/assertions.h
+++ test_helpers/assertions.h
@@ -1,15 +1,34 @@
 #include <iostream>
 #include <string>
 
+
+void printTestResultMessage(bool succeeded, std::string testName) {
+    if (succeeded) {
+        std::cout << "Test SUCCEEDED for " << testName << std::endl; 
+    } else {
+        std::cout << "Test FAILED for " << testName << std::endl;
+    }
+}
+
 void assertEquals(float * expected, float * actual, int coreSize,
                   std::string testName) {
+    bool succeded = true;
     for (int i = 0; i < coreSize; i++) {
         if (expected[i] != actual[i]) {
             std::cout << "Values were different at index " << i << ": expected {"
             << expected[i] << "} but actual was {" << actual[i] << "}\n";
-            std::cout << "Test FAILED for " << testName << std::endl;
-            return;
+            succeded = false;
         }
     }
-    std::cout << "Test SUCCEEDED for " << testName << std::endl;  
+    printTestResultMessage(succeded, testName); 
+}
+
+void assertEquals(int expected, int actual, std::string testName) {
+    bool succeeded = true;
+    if (expected != actual) {
+        std::cout << "Values are different: expected {" << expected << 
+        "} but actual was {" << actual << "}\n";
+        succeeded = false;
+    }
+    printTestResultMessage(succeeded, testName);
 }
