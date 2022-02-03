ff2e492d47b7d44911297e4768be4e069b1bc063
simmsim <simona.paulau@gmail.com>
2022-02-03 19:47:55 +0000

    Add function when description not provided

--- utility/print_exec_time_to_file.h
+++ utility/print_exec_time_to_file.h
@@ -1,11 +1,19 @@
 #include <fstream>
 #include <chrono>
+#include <string>
+
+void print_to_file(std::chrono::steady_clock::time_point begin,
+                   std::chrono::steady_clock::time_point end,
+                   char * fileName, std::string description) {
+    std::ofstream outfile;
+    outfile.open(fileName, std::ios_base::app);
+    outfile << description << "[ms] " << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << "\n";
+    outfile << description << "[µs] " << std::chrono::duration_cast<std::chrono::microseconds> (end - begin).count() << "\n";
+}
 
 void print_to_file(std::chrono::steady_clock::time_point begin,
                    std::chrono::steady_clock::time_point end,
                    char * fileName) {
-    std::ofstream outfile;
-    outfile.open(fileName, std::ios_base::app);
-    outfile << "Elapsed[ms] " << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() << "\n";
-    outfile << "Elapsed[µs] " << std::chrono::duration_cast<std::chrono::microseconds> (end - begin).count() << "\n";
+    std::string description = "Elapsed";
+    print_to_file(begin, end, fileName, description);
 }
