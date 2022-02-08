37d8a754e2b3cf93b2c3fcd4b3e759afb1a30cb8
simmsim <simona.paulau@gmail.com>
2022-02-08 12:43:50 +0000

    Add function to utility class for printing average of event measurements to file

--- utility/print_exec_time_to_file.h
+++ utility/print_exec_time_to_file.h
@@ -1,6 +1,8 @@
 #include <fstream>
 #include <chrono>
 #include <string>
+#include <vector>
+#include <cmath>
 
 void print_to_file(std::chrono::steady_clock::time_point begin,
                    std::chrono::steady_clock::time_point end,
@@ -16,4 +18,20 @@ void print_to_file(std::chrono::steady_clock::time_point begin,
                    char * fileName) {
     std::string description = "Elapsed";
     print_to_file(begin, end, fileName, description);
+}
+
+void print_average_to_file(std::vector<double> measurementsVec, char * fileName, std::string description) {
+    long long measurementMs = 0.0;
+    long long measurementMcs = 0.0;
+    for (double measurement: measurementsVec) {
+        measurementMs += round(measurement)*1.0e-6;
+        measurementMcs += round(measurement)*1.0e-3;
+    }
+    int countOfMeasurements = measurementsVec.size();
+
+    std::ofstream outfile;
+    outfile.open(fileName, std::ios_base::app);
+
+    outfile << "\n" << description << "[ms] " << measurementMs/countOfMeasurements << "\n";
+    outfile << description << "[µs] " << measurementMcs/countOfMeasurements << "\n\n";
 }
