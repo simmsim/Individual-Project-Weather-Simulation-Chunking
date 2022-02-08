a74d8de703cb7bd9b8f14c5755e1b566d8ec1029
simmsim <simona.paulau@gmail.com>
2022-02-08 20:44:16 +0000

    Add macro for whether to enable event performance gathering or not

--- sor-c-reference/SOR_API_Version.cpp
+++ sor-c-reference/SOR_API_Version.cpp
@@ -3,6 +3,12 @@
 #include "../utility/domain_generator.h"
 #include "../utility/print_exec_time_to_file.h"
 
+#if defined(MEASURE_EVENT_PERFORMANCE) 
+    #define PROFILING_ENABLED 1
+#else
+    #define PROFILING_ENABLED 0
+#endif
+
 int main(int argc, char* argv[]) {
     SimulationRange coreRange = SimulationRange(ip, jp, kp);
     SimulationRange chunkRange = SimulationRange(cip, cjp, ckp);
@@ -34,14 +40,21 @@ int main(int argc, char* argv[]) {
         std::cout << "Simulation execution failed with error code " << err << "\n";
         return -1;
     }
-   // std::cout << p[F3D2C(ip+2,jp+2, 0,0,0,ip/2,jp/2,kp/2)] << "\n";
+   
+   float * actualSimulationArea = simulation.getSimulationArea().p;
+   float actualValue =  actualSimulationArea[F3D2C(ip, jp, 0,0,0, 1,1,1)];
+   std::cout << "Value was " << actualValue << "\n";
 
     char * fileName = (char *)malloc(strlen(argv[1]) + 1);
     strcpy(fileName, argv[1]);
 
     print_to_file(beginWithSetup, end, fileName, "ElapsedOCL");
     print_to_file(beginSimulationOnly, end, fileName, "ElapsedSimulationOnly");
-    print_average_to_file(simulation.getPerformanceMeasurements().clKernelExecution, fileName, "Core kernel execution");
+    if (PROFILING_ENABLED) {
+        print_avg_to_file(simulation.getPerformanceMeasurements().clKernelExecution, fileName, "Core kernel execution");
+        print_to_file(simulation.getPerformanceMeasurements().clReadFromDevice, fileName, "Read from device");
+        print_total_write_avg_to_file(simulation.getPerformanceMeasurements().clWriteToDevice, fileName, "Write to device");
+    }
 
     free(fileName);
     free(p);
