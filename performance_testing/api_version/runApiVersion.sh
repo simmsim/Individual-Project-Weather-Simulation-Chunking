6d3109a31b7f52f5554d9fce66b7fc825bf315fe
simmsim <simona.paulau@gmail.com>
2022-02-18 16:21:52 +0000

    Update api bash script so that it collects and prints averages of all the time measurements

--- performance_testing/api_version/runApiVersion.sh
+++ performance_testing/api_version/runApiVersion.sh
@@ -1,5 +1,65 @@
 #!/bin/bash
 
+measurements=(0,0,0,0,0,0,0,0,0,0)
+
+declare -A nameIndexDictionary
+nameIndexDictionary["ElapsedOCL[ms]"]=0
+nameIndexDictionary["ElapsedOCL[µs]"]=1
+nameIndexDictionary["ElapsedSimulationOnly[ms]"]=2
+nameIndexDictionary["ElapsedSimulationOnly[µs]"]=3
+nameIndexDictionary["Kernel[ms]"]=4
+nameIndexDictionary["Kernel[µs]"]=5
+nameIndexDictionary["Write[ms]"]=6
+nameIndexDictionary["Write[µs]"]=7
+nameIndexDictionary["Read[ms]"]=8
+nameIndexDictionary["Read[µs]"]=9
+
+declare -a orders
+orders+=( "ElapsedOCL[ms]" )
+orders+=( "ElapsedOCL[µs]" )
+orders+=( "ElapsedSimulationOnly[ms]" )
+orders+=( "ElapsedSimulationOnly[µs]" )
+orders+=( "Kernel[ms]" )
+orders+=( "Kernel[µs]" )
+orders+=( "Write[ms]" )
+orders+=( "Write[µs]" )
+orders+=( "Read[ms]" )
+orders+=( "Read[µs]" )
+
+function addMeasurement() {
+    index="$1"
+    if [[ $((${index}%2)) -eq 0 ]]; then
+        currentSumMili=${measurements[${index}]}
+        measurements[${index}]=$(( currentSumMili+"$2" ))
+    else
+        currentSumMicro=${measurements[${index}]}
+        measurements[${index}]=$(( currentSumMicro+"$2" ))
+    fi
+}
+
+function produce_average() {
+    while read -r line
+    do
+        measurementName="$(echo "$line" | awk '{print $1;}')"
+        if [[ -z "$measurementName" ]]; then
+            continue
+        fi
+        value="$(echo "$line" | awk '{print $2;}')"
+        startIndex=${nameIndexDictionary[${measurementName}]}
+        if [[ ! -z "$startIndex" ]]; then
+            addMeasurement ${startIndex} ${value}
+        fi
+    done < <(tail -n +6 $1)
+
+    for i in "${orders[@]}"; do
+        keyName=$i
+        indexValue=${nameIndexDictionary[${keyName}]}
+        measurementValue=${measurements[${indexValue}]}
+        average=$(( measurementValue/$2 ))
+        echo "Average ${keyName} ${average}" >> $1
+    done
+}
+
 ### Construct fileName and echo info to console.
 timestamp=$(date +%d-%m-%Y_%H-%M-%S)
 deviceName="CPU"
@@ -7,14 +67,14 @@ fileNameStem="apiVersionLog_"
 location="../performance_testing/api_version/"
 fileName="${fileNameStem}${deviceName}_${timestamp}.txt"
 
-numberOfRuns=10
-numberOfIterations=50
+numberOfRuns=5
+numberOfIterations=5
 # 0 for CPU and 1 for GPU
 device=0
-ip=150
-jp=150
-kp=90
-cip=30
+ip=152
+jp=152
+kp=92
+cip=150
 cjp=150
 ckp=90
 
@@ -46,7 +106,8 @@ echo "const int cjp = ${cjp};" >> ${propertiesFile}
 echo "const int ckp = ${ckp};" >> ${propertiesFile}
 
 ### Compile
-g++ -std=c++11 -O3 ../../sor-c-reference/SOR_API_Version.cpp -o ../../sor-c-reference/SOR_API_Version ../../Simulation.cpp ../../OCLSetup.cpp ../../ErrorHelper.cpp -lOpenCL
+### Add -DMEASURE_EVENT_PERFORMANCE if want to measure execution time of OpenCL events
+g++ -std=c++11 -O3 -DMEASURE_EVENT_PERFORMANCE ../../sor-c-reference/SOR_API_Version.cpp -o ../../sor-c-reference/SOR_API_Version ../../Simulation.cpp ../../OCLSetup.cpp ../../ErrorHelper.cpp -lOpenCL
 
 ### Run
 for ((i=0; i<$numberOfRuns; i++)); do 
@@ -59,7 +120,7 @@ echo -ne "Runs have finished, and the file ${fileName} "
 linesInFile="$(cat "$fileName" | wc -l)"
 if test -f "${fileName}"; then
     echo "was SUCCESSFULLY populated."
-    #produce_average ${fileName} ${numberOfRuns}
+    produce_average ${fileName} ${numberOfRuns}
 else
     echo "FAILED to be populated."
 fi
