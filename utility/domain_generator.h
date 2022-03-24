c9d6a0a40a9dd6392f7552ea963215e85cb8b26a
simmsim <simona.paulau@gmail.com>
2022-03-24 10:48:57 +0000

    Cleanup domain generator

--- utility/domain_generator.h
+++ utility/domain_generator.h
@@ -8,23 +8,14 @@ void generateDomain(float * p0, float * rhs, SimulationRange simulationRange) {
     int jDim = simulationRange.getDimSizes()[1];
     int kDim = simulationRange.getDimSizes()[2];
 
-    //Simple numbers that are easy to reason about when testing.
-    //  for (int z = 0; z < simulationRange.getSimulationSize(); z++) {
-    //      p0[z] = z;
-    //     // std::cout << " " << p0[z];
-    //  }
-    //std::cout << "\n\n";
-
     for (int i = 0;i < iDim;i += 1) {
         for (int j = 0;j < jDim;j += 1) {
             for (int k = 0;k < kDim;k += 1) {
                 rhs[F3D2C(iDim,jDim,0,0,0,i,j,k)] = 0.1+((float)(i+1)*(j+1)*(k+1))/((float)(iDim)*(jDim)*(kDim));
                 p0[F3D2C(iDim,jDim,0,0,0,i,j,k)] = ((float)(i+1)*(j+1)*(k+1))/((float)(iDim)*(jDim)*(kDim));
-                //std::cout << " " << p0[F3D2C(iDim,jDim,0,0,0,i,j,k)];
             }
         }
     }
-    //std::cout << "\n\n";
 }
 
 void generateSimpleDomain(float * p0, float * rhs, SimulationRange simulationRange) {
